a42394cca9258d2ef7dbf1fde6fd99fb4af64ec6
DeniArif <deniarifwibowo82@gmail.com>
2023-04-03 14:40:34 +0700

    comit

--- ActivityStructure_188_D/ActivityStructure_188_D.cpp
+++ ActivityStructure_188_D/ActivityStructure_188_D.cpp
@@ -17,4 +17,16 @@ int main()
 	mhs1.nama = "Nabila";
 	mhs1.alamat = "brebes";
 	mhs1.umur = 17;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nNama : " << mhs1.nama;
 };
